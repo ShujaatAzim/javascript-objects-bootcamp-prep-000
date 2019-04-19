56c9f57c5a9b7362b2b2c422674eb215ed295cba
Shujaat Azim <shujaat.azim@gmail.com>
2019-04-19 18:07:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,5 +7,6 @@ function updatePlaylist(playlist, artist, title) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return delete playlist.artistName;
+  delete playlist.artistName;
+  return playlist;
 }
